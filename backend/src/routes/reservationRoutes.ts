8d4c5fa9103aff8b8a37755cb4d21d2defc33fe6
bartekfenicki <bartek.fenicki@gmail.com>
2025-12-09 15:23:08 +0100

    EMIAL RES

--- backend/src/routes/reservationRoutes.ts
+++ backend/src/routes/reservationRoutes.ts
@@ -94,6 +94,6 @@ router.put("/:id", updateExistingReservation);
  */
 router.delete("/:id", deleteExistingReservation);
 
-router.get("/:id/by-email", sendReservationByEmail);
+router.post("/:id/by-email", sendReservationByEmail);
 
 export default router;
